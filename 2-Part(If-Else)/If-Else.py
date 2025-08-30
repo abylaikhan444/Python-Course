39e1742d900b1105e9479455200ff312fedc7ca4
Abylaikhan444 <fifa77882@gmail.com>
2025-08-30 23:21:46 +0500

    first commit about of Data Parts

--- 2-Part(If-Else)/If-Else.py
+++ 2-Part(If-Else)/If-Else.py
@@ -531,21 +531,21 @@
 # .................................................................................................................................................
 
 
-# a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
 
-# if a1 + b1 == a2 + b2 or a2 + b2 == a1 + b1:
-#     print('YES')
-# elif a1 - b1 == a2 - b2:
-#     print('YES')
-# elif a1 == a2 or b1 == b2:
-#     print('YES')
-# elif a1 == a2 and (b1 - 1 == b2 or b1 + 1 == b2):
-#     print('YES')
-# elif b1 == b2 and (a1 - 1 == a2 or a1 + 1 == a2):
-#     print('YES')
-# elif (a1 + 1 == a2 and b1 - 1 == b2) or (a1 - 1 == a2 and b1 + 1 == b2):
-#     print('YES')
-# elif (a1 + 1 == a2 and b1 + 1 == b2) or (a1 - 1 == a2 and b1 - 1 == b2):
-#     print('YES')
-# else:
-#     print('NO')
+if a1 + b1 == a2 + b2 or a2 + b2 == a1 + b1:
+    print("YES")
+elif a1 - b1 == a2 - b2:
+    print("YES")
+elif a1 == a2 or b1 == b2:
+    print("YES")
+elif a1 == a2 and (b1 - 1 == b2 or b1 + 1 == b2):
+    print("YES")
+elif b1 == b2 and (a1 - 1 == a2 or a1 + 1 == a2):
+    print("YES")
+elif (a1 + 1 == a2 and b1 - 1 == b2) or (a1 - 1 == a2 and b1 + 1 == b2):
+    print("YES")
+elif (a1 + 1 == a2 and b1 + 1 == b2) or (a1 - 1 == a2 and b1 - 1 == b2):
+    print("YES")
+else:
+    print("NO")
