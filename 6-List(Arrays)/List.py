6d128d730b53b91afcb4c23f5908f55bdb696378
Abylaikhan444 <fifa77882@gmail.com>
2025-10-22 13:18:46 -0700

    redacted

--- 6-List(Arrays)/List.py
+++ 6-List(Arrays)/List.py
@@ -90,10 +90,33 @@
 # .................................................................................................................................................
 
 
-numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2, 12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
+# numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2, 12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
 
-last_element = numbers[len(numbers) - 1]
+# last_element = numbers[len(numbers) - 1]
+#
+# print(len(numbers))
+# print(last_element)
+# print(numbers[::-1])
+#
+# if 5 in numbers and 17 in numbers:
+#     print('YES')
+# else:
+#     print('NO')
+#
+# del numbers[len(numbers) - 1]
+# del numbers[0]
+#
+# print(numbers)
 
-print(len(numbers))
-print(last_element)
-print(numbers[::-1])
+
+# .................................................................................................................................................
+
+
+n = int(input())
+words = []
+
+for i in range(n):
+    word = input()
+    words.append(word)
+
+print(words)
