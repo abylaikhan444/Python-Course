267aa7fd5fc48a36a8fc5cf98822e422510eb893
Abylaikhan444 <fifa77882@gmail.com>
2025-11-12 12:48:03 +0500

    redacted

--- Mini_projects/RandomPart.py
+++ Mini_projects/RandomPart.py
@@ -1,8 +1,27 @@
-import random
+from random import *
 
-for i in range(5):
-    print(random.randint(1, 30))
+# for i in range(5):
+#     print(random.randint(1, 30))
+#
+#     print(random.randint(1, 30))
+#
+#     print(random.randint(1, 30))
 
-    print(random.randint(1, 30))
+# Угадайка
 
-    print(random.randint(1, 30))
+print('Hello, bro!> Do you want to play choice?')
+
+def choice():
+    flag = True
+    choice_var = input('Please enter your answer (yes / no)').lower()
+    if choice_var == 'yes':
+        print('Ok, lets go to play!')
+    else:
+        print('Thank you for playing!')
+
+def main():
+    rand_number = randint(1, 100)
+    print('Please > You can try to guess number!')
+    user_num = int(input('Enter number: '))
+    while user_num != rand_number:
+        if
